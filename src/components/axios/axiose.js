d4cbb81098125ec61ac789c29f0e91f039e80968
faheemfazal <faheemfazalpkv@gmail.com>
2024-05-02 13:59:27 +0530

    host url setup

--- src/components/axios/axiose.js
+++ src/components/axios/axiose.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosUser = axios.create({
-    baseURL:'http://localhost:4000/',
+    baseURL:'https://backendbookusnow.vercel.app/',
     headers: {
         "Content-Type": "application/json",
       },
